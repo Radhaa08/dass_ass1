f0731b5ce37eacca819b4bde3e647c16bd2650b6
Radhaaa <156424548+Radhaa08@users.noreply.github.com>
2025-07-07 16:37:29 +0530

    Update Cart.js

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -45,7 +45,7 @@ function Cart() {
                     description: item.description,
                 };
 
-                const response = await fetch("http://localhost:8080/api/orders", {
+                const response = await fetch("/api/orders", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
