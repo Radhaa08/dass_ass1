a16b60029811344562108e7d2dc1cf51521de3c1
Radhaa08 <radha.r1525@gmail.com>
2025-07-07 15:37:11 +0530

    need to deploy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -9,6 +10,7 @@ const itemRoutes = require("./routes/items");
 const orderRoutes = require("./routes/orders");
 const chatbotRoutes = require("./routes/chatbot");
 // database connection
+console.log(__dirname);
 connection();
 
 // middlewares
@@ -21,6 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/chat", chatbotRoutes);
-
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+// app.use(express.static("/client/build"));
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
